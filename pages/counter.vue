132a2c90030ed289e0d535ff185d0ecc1bd8ff68
m-sato <ssmmaagg36@gmail.com>
2023-10-29 18:24:13 +0900

    カウンターアプリ実装

--- pages/counter.vue
+++ pages/counter.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { counter, updateCounter } = useCounter();
+</script>
 <template>
   <div>
     <h2>カウンターアプリ</h2>
     <hr />
+    <div>
+      <h3>Counter: {{ counter }}</h3>
+      <button @click="updateCounter('+')">+</button>
+      <button @click="updateCounter('-')">-</button>
+      <button @click="updateCounter('')">リセット</button>
+    </div>
   </div>
 </template>
+<style>
+h3 {
+  margin: 20px auto;
+}
+button {
+  display: inline-block;
+  width: 100px;
+  height: 40px;
+  margin-right: 4px;
+}
+</style>
